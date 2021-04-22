79702604ff64758bb31827fc5957b6d10764e9ad
niro <roquenikko@gmail.com>
2021-04-21 18:03:36 -0700

    firebase

--- src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyC2yYNbvwvcvclA4DCGk8tIDFXNORItvsI",
-    authDomain: "niro-fd1ef.firebaseapp.com",
-    databaseURL: "niro-fd1ef",
-    projectId: "niro-fd1ef",
-    storageBucket: "niro-fd1ef.appspot.com",
-    messagingSenderId: "375508463409"
-  };
